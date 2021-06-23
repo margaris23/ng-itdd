9ffcb830ec430f56f0201972e50fa9d587d24263
Nikolaos Margaris <nma@anixe.io>
2021-06-23 17:30:56 +0300

    step 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { Country } from './countries/country.model';
 import { CountryService } from './country.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-itdd';
+  countries$: Observable<Country[]>;
 
-  constructor(private countryService: CountryService) { }
+  constructor(private countryService: CountryService) {}
+
+  ngOnInit(): void {
+    this.countries$ = this.countryService.list();
+  }
 }
--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { Country } from './countries/country.model';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountryService {
+  constructor() {}
 
-  constructor() { }
+  list(): Observable<Country[]> {
+    return of([]);
+  }
 }
